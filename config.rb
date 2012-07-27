8d4e7cdab8ac8ad47c31d30d9b50dd0de318a30f
cwebbdesign <christopherwdesign@gmail.com>
2012-07-27 14:44:02 +0200

    Initial setup.

---
+++ config.rb
@@ -0,0 +1,102 @@
+require "kss"
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install compass-susy-plugin
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+
+["styleguide/*"].each do |name|
+  @styleguide = Kss::Parser.new("source/stylesheets")
+  page "#{name}", :layout => "layouts/styleguide"
+end
+
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# KSS: Generates a styleguide block. A little bit evil with @_out_buf, but
+  # if you're using something like Rails, you can write a much cleaner helper
+  # very easily.
+  def styleguide_block(section, &block)
+    @section = @styleguide.section(section)
+    @example_html = kss_capture{ block.call }
+    @_out_buf << partial('partials/block')
+  end
+
+  # KSS: Captures the result of a block within an erb template without spitting
+  # it to the output buffer.
+  def kss_capture(&block)
+    out, @_out_buf = @_out_buf, ""
+    yield
+    @_out_buf
+  ensure
+    @_out_buf = out
+  end
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
